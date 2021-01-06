5ae2f1c23be3ee7090a37fb666b014dc10d55c19
jkups <o.johnkupoluyi@gmail.com>
2021-01-06 17:45:45 +1100

    reservation update

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,14 @@ class Flight < ApplicationRecord
   has_many :reservations
   has_and_belongs_to_many :users, through: :reservations, optional: true
 
+  def generate_seat_map
+    row = self.airplane.row
+    cols = self.airplane.cols
+
+    row.times do
+      cols.times do
+      end
+    end
+    
+  end
 end
